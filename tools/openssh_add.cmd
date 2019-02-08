1632907fbe85c10b6562ab653242292d1536e910
Julien Batonnet <julien.batonnet@marinepress.com>
2019-02-08 08:55:16 -0500

    3.0.8

--- tools/openssh_add.cmd
+++ tools/openssh_add.cmd
@@ -3,4 +3,7 @@ echo.
 echo SourceTree is loading your SSH key into the agent for authentication
 echo Please enter your passphrase if prompted to do so
 echo.
-ssh-add %*
+set x=%*
+For %%_ In (%x%) DO (
+ssh-add %%_
+)
